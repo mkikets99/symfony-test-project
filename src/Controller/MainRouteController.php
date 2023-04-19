3633d6dc76509e00aedc5c4ac10acf479346e62a
Mykola Kikets <mykola.kikets@gmail.com>
2023-04-19 16:35:25 +0300

    moving link gen to separate func

--- src/Controller/MainRouteController.php
+++ src/Controller/MainRouteController.php
@@ -19,6 +19,17 @@ class MainRouteController extends AbstractController
     array('href' => "/", 'label' => 'Home'),
     array('href' => "/articles", 'label' => 'Articles')
   );
+
+  private function link_gen(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger)
+  {
+    $userID = $request->getSession()->get('user_id');
+    $additional = array();
+    if ($userID != null)
+      array_push($additional, array('href' => "/user/home", 'label' => 'User Space'));
+    array_push($additional, array('href' => (($userID == null) ? "/login" : "/logout"), 'label' => ($userID == null) ? "Login" : "Logout"));
+    return array_merge($this->default_links, $additional);
+  }
+
   #[Route('/', name: 'home')]
   public function home(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger)
   {
@@ -32,29 +43,60 @@ class MainRouteController extends AbstractController
     }
 
     return $this->render('pages/home.html.twig', array(
-      'links' => array_merge($this->default_links, array(
-        array('href' => (($userID == null) ? "/login" : "/logout"), 'label' => ($userID == null) ? "Login" : "Logout")
-      ))
+      'links' => $this->link_gen($request, $entityManager, $logger)
     ));
     // return new Response('Hello World');
     // return $this->render('home.html.twig');
   }
 
   #[Route('/article', name: 'article')]
-  public function article()
+  public function article(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger)
   {
-    return $this->render('pages/article.html.twig');
+    $userID = $request->getSession()->get('user_id');
+    if ($userID !=  null) {
+      $user = $entityManager->getRepository(User::class)->find($userID);
+
+      $user->setLastAction(new \DateTime());
+      $entityManager->persist($user);
+      $entityManager->flush();
+    }
+
+    return $this->render('pages/article.html.twig', array(
+      'links' => $this->link_gen($request, $entityManager, $logger)
+    ));
   }
 
   #[Route('/articles', name: 'articles')]
-  public function articles()
+  public function articles(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger)
   {
-    return $this->render('pages/articles.html.twig');
+    $userID = $request->getSession()->get('user_id');
+    if ($userID !=  null) {
+      $user = $entityManager->getRepository(User::class)->find($userID);
+
+      $user->setLastAction(new \DateTime());
+      $entityManager->persist($user);
+      $entityManager->flush();
+    }
+
+    return $this->render('pages/articles.html.twig', array(
+      'links' => $this->link_gen($request, $entityManager, $logger)
+    ));
   }
 
   #[Route('/user/home', name: 'user-home')]
-  public function userHome()
+  public function userHome(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger)
   {
-    return $this->render('pages/user/home.html.twig');
+    $userID = $request->getSession()->get('user_id');
+    if ($userID !=  null) {
+      $user = $entityManager->getRepository(User::class)->find($userID);
+
+      $user->setLastAction(new \DateTime());
+      $entityManager->persist($user);
+      $entityManager->flush();
+    }
+
+    return $this->render('pages/user/home.html.twig', array(
+      'links' => $this->link_gen($request, $entityManager, $logger)
+    ));
   }
 }
